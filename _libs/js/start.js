08a946030a87f5cf874cb5b834c74fa9a22da2a3
DennisNevback <dennis.nevback@gmail.com>
2025-09-15 10:49:52 +0200

    iframe working

--- _libs/js/start.js
+++ _libs/js/start.js
@@ -1,4 +1,5 @@
-// Include the scripts
+//* Include the scripts
+/*
 let myPath = [...document.querySelectorAll('script')]
   .map(({ src }) => src)
   .filter(x => x.includes('/start.js'))[0]
@@ -12,6 +13,35 @@ let _scripts = [
 document.write(_scripts);
 setTimeout(function start() {
   if (!('brython' in window)) { setTimeout(start, 10); return; }
-  brython(1);
+  brython(0);
   console.clear();
 });
+*/
+
+// Include the scripts dynamically
+let myPath = [...document.querySelectorAll('script')]
+  .map(({ src }) => src)
+  .filter(x => x.includes('/start.js'))[0]
+  .slice(0, -8);
+
+let libs = [
+  'jquery.js',
+  'bootstrap.bundle.min.js',
+  'brython.js',
+  'brython_stdlib.js'
+];
+
+function loadScript(src, callback) {
+  let script = document.createElement('script');
+  script.src = src;
+  script.onload = callback;
+  document.head.appendChild(script);
+}
+
+(function loadLibs(i = 0) {
+  if (i >= libs.length) {
+    if (typeof brython !== 'undefined') brython(0);
+    return;
+  }
+  loadScript(myPath + libs[i], () => loadLibs(i + 1));
+})();
